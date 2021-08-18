22f136b8d0bb670a7bf37c234b44ce1c7f36888d
Nastya10 <48963234+Nastya10@users.noreply.github.com>
2021-08-18 13:45:15 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,30 +9,42 @@ int main(void)
     float d = 0;
     float root1 = 0, root2 = 0;
 
-    printf("Программа решает квадратные уравнения\n");
-    printf("Введите коэффиценты:");
+    printf("The program solves quadratic equations\n");
+    printf("Enter the coefficients:");
     scanf("%f %f %f", &a, &b, &c);
 
-    d =  pow(b, 2) - 4 * a * c;
-
-    if(d < 0)
+    if(a == 0)
     {
-        printf("Квадратное уравнение не имеет корней\n");
-    }
-    else
-    {
-        root1 =  ((- b) + sqrtf(d)) / (2 * a);
-        root2 = ((- b) - sqrtf(d)) / (2 * a);
-        if(root1 == root2)
+        if(b == 0 || c == 0)
         {
-            printf("У квадратного уравнения есть один корень: %f\n", root1);
+                printf("The quadratic equation has one root: %f\n", 0);
         }
         else
         {
-            printf("У квадратного уравнения есть два корня\nПервый: %f\nВторой: %f\n", root1, root2);
+            printf("The quadratic equation has one root: %f\n", (c / b));
         }
     }
+    else
+    {
+        d =  pow(b, 2) - 4 * a * c;
 
+        if(d < 0)
+        {
+            printf("The quadratic equation has no roots\n");
+        }
+        else
+        {
+            root1 =  ((- b) + sqrtf(d)) / (2 * a);
+            root2 = ((- b) - sqrtf(d)) / (2 * a);
+            if(root1 == root2)
+            {
+                printf("The quadratic equation has one root: %f\n", root1);
+            }
+            else
+            {
+                printf("The quadratic equation has two roots\nFirst: %f\nSecond: %f\n", root1, root2);
+            }
+        }
+    }
     return EXIT_SUCCESS;
 }
-
