c6750033ff9da6f558922e5cd5f4357c034fecd8
Nastya10 <48963234+Nastya10@users.noreply.github.com>
2021-08-19 16:40:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 #include <math.h>
 #include "TXLib.h"
 
+int solution_linear(float coef1, float coef2, float* root);
+int solution_square(float a, float b, float c, float* root1, float* root2);
+
+const int INFINITY_SOLUTIONS = 3;
+
 int main(void)
 {
     float a = 0, b = 0, c = 0;
@@ -13,16 +18,19 @@ int main(void)
     printf("Enter the coefficients:");
     scanf("%f %f %f", &a, &b, &c);
 
+    int n_roots = solution_square(a, b, c, &root1, &root2);
+    printf("%d, %f", n_roots, root1);
+
+    return EXIT_SUCCESS;
+}
+
+int solution_square(float a, float b, float c, float* root1, float* root2)
+{
+    float d = 0;
+
     if(a == 0)
     {
-        if(b == 0 || c == 0)
-        {
-                printf("The quadratic equation has one root: %f\n", 0);
-        }
-        else
-        {
-            printf("The quadratic equation has one root: %f\n", (c / b));
-        }
+        return solution_linear(b, c, *root1);
     }
     else
     {
@@ -30,21 +38,41 @@ int main(void)
 
         if(d < 0)
         {
-            printf("The quadratic equation has no roots\n");
+            return 0;
         }
         else
         {
-            root1 =  ((- b) + sqrtf(d)) / (2 * a);
-            root2 = ((- b) - sqrtf(d)) / (2 * a);
-            if(root1 == root2)
+            *root1 =  ((- b) + sqrtf(d)) / (2 * a);
+            *root2 = ((- b) - sqrtf(d)) / (2 * a);
+            if(*root1 == *root2)
             {
-                printf("The quadratic equation has one root: %f\n", root1);
+                return 1;
             }
             else
             {
-                printf("The quadratic equation has two roots\nFirst: %f\nSecond: %f\n", root1, root2);
+                return 2;
             }
         }
     }
-    return EXIT_SUCCESS;
+}
+
+int solution_linear(float coef1, float coef2, float* root)
+{
+    if(coef1 == 0)
+    {
+        if(coef2 == 0)
+        {
+            return INFINITY_SOLUTIONS;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+    else
+    {
+        *root = (-coef2)/coef1;
+        return 1;
+    }
 }
