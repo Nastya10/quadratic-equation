6933c985651062d21228c78930cf639b99cd5b8f
Nastya10 <48963234+Nastya10@users.noreply.github.com>
2021-08-20 16:01:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -28,8 +28,8 @@ const float EPSILON = 10E-8;
  * @param[in]   a quadratic coefficient
  * @param[in]   b linear coefficient
  * @param[in]   c free member
- * @param[out]  root1
- * @param[out]  root2
+ * @param[out]  root1 the larger root of the equation is placed
+ * @param[out]  root2 the smaller root of the equation is placed
  *
  * @return Number of roots
  *
@@ -42,9 +42,9 @@ int solve_square(float a, float b, float c, float* root1, float* root2);
 /**
  * Solves a square equation bx + c = 0
  *
- * @param[in]  b coefficient
- * @param[in]  c coefficient
- * @param[out]  root
+ * @param[in]   b linear coefficient
+ * @param[in]   c coefficient
+ * @param[out]  free member
  *
  * @return Number of roots
  *
@@ -52,7 +52,7 @@ int solve_square(float a, float b, float c, float* root1, float* root2);
  * @retval 1
  * @retval INF_ROOTS in case of infinite number of roots
  */
-int solve_linear(float coef1, float coef2, float* root);
+int solve_linear(float float1, float float2, float* root);
 
 /**
  * Checks the equality of two floats
@@ -60,7 +60,7 @@ int solve_linear(float coef1, float coef2, float* root);
  * @param[in]  float1
  * @param[in]  float2
  *
- * @return Is it true that the numbers are equal
+ * @return Is it true that the floats are equal
  */
 bool floats_are_equal(float float1, float float2);
 
