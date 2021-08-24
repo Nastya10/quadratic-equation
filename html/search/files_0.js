a7d84f4fda80ba5ff68f0cca0501f1b2aaace278
Nastya10 <48963234+Nastya10@users.noreply.github.com>
2021-08-24 22:23:37 +0300

    Delete html directory

--- html/search/files_0.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['floats_2ecpp_0',['floats.cpp',['../floats_8cpp.html',1,'']]],
-  ['floats_2eh_1',['floats.h',['../floats_8h.html',1,'']]],
-  ['functions_5ffo_5ffloats_2eh_2',['functions_fo_floats.h',['../functions__fo__floats_8h.html',1,'']]]
-];
