364e0b1273a48bdfd8d77f40285ddf18acd42c75
Nastya10 <48963234+Nastya10@users.noreply.github.com>
2021-08-24 14:37:38 +0300

    Add files via upload

--- test.h
+++ test.h
@@ -7,6 +7,8 @@
 #ifndef TEST_H_INCLUDED
 #define TEST_H_INCLUDED
 
+#include "solver.h"
+
 int test_solve_square(void);
 int test_solve_square_roots_quantity(float a, float b, float c, enum roots_num correct_n_roots);
 int test_solve_square_roots(float a, float b, float c, float root1_expected, float root2_expected);
