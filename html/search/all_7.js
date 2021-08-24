33da70a8749fa75ef02509ec83f34abc0d82d1b7
Nastya10 <48963234+Nastya10@users.noreply.github.com>
2021-08-24 21:05:58 +0300

    docs

---
+++ html/search/all_7.js
@@ -0,0 +1,13 @@
+var searchData=
+[
+  ['test_0',['TEST',['../main_8cpp.html#ab946e2e7f7679350627acfded8e2658b',1,'main.cpp']]],
+  ['test_2ecpp_1',['test.cpp',['../test_8cpp.html',1,'']]],
+  ['test_2eh_2',['test.h',['../test_8h.html',1,'']]],
+  ['test_5fsolve_5flinear_3',['test_solve_linear',['../test_8cpp.html#a806c32463bc9784d1563b090b189f83e',1,'test_solve_linear(void):&#160;test.cpp'],['../test_8h.html#a806c32463bc9784d1563b090b189f83e',1,'test_solve_linear(void):&#160;test.cpp']]],
+  ['test_5fsolve_5flinear_5froot_4',['test_solve_linear_root',['../test_8cpp.html#a6d477918c3dcd84fe5554ba768727f9d',1,'test_solve_linear_root(float b, float c, float root_expected):&#160;test.cpp'],['../test_8h.html#a6d477918c3dcd84fe5554ba768727f9d',1,'test_solve_linear_root(float b, float c, float root_expected):&#160;test.cpp']]],
+  ['test_5fsolve_5flinear_5froots_5fquantity_5',['test_solve_linear_roots_quantity',['../test_8cpp.html#af52db0a3c7776156ee809e4b58c7fea8',1,'test_solve_linear_roots_quantity(float b, float c, enum roots_num correct_n_roots):&#160;test.cpp'],['../test_8h.html#af52db0a3c7776156ee809e4b58c7fea8',1,'test_solve_linear_roots_quantity(float b, float c, enum roots_num correct_n_roots):&#160;test.cpp']]],
+  ['test_5fsolve_5fquadratic_6',['test_solve_quadratic',['../test_8cpp.html#a92ba28138a53c090f4931d74becf2ea2',1,'test_solve_quadratic(void):&#160;test.cpp'],['../test_8h.html#a92ba28138a53c090f4931d74becf2ea2',1,'test_solve_quadratic(void):&#160;test.cpp']]],
+  ['test_5fsolve_5fquadratic_5froots_7',['test_solve_quadratic_roots',['../test_8cpp.html#a20ad6b6028fce7b7bd8811955da467ba',1,'test_solve_quadratic_roots(float a, float b, float c, float root1_expected, float root2_expected):&#160;test.cpp'],['../test_8h.html#a20ad6b6028fce7b7bd8811955da467ba',1,'test_solve_quadratic_roots(float a, float b, float c, float root1_expected, float root2_expected):&#160;test.cpp']]],
+  ['test_5fsolve_5fquadratic_5froots_5fquantity_8',['test_solve_quadratic_roots_quantity',['../test_8cpp.html#a6961b94fe24b5ad2cd7fb22531554919',1,'test_solve_quadratic_roots_quantity(float a, float b, float c, enum roots_num correct_n_roots):&#160;test.cpp'],['../test_8h.html#a6961b94fe24b5ad2cd7fb22531554919',1,'test_solve_quadratic_roots_quantity(float a, float b, float c, enum roots_num correct_n_roots):&#160;test.cpp']]],
+  ['two_5froots_9',['TWO_ROOTS',['../solver_8h.html#a4bb452ab79aaa95e2cf7f2453824a09da876622d3c0b008da13a3685c64714e81',1,'solver.h']]]
+];
