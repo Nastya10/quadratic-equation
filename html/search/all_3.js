905dd1c0975c2a3bc421be7f0b30bbcec16f0418
Nastya10 <48963234+Nastya10@users.noreply.github.com>
2021-08-24 20:40:58 +0300

    html

---
+++ html/search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['no_5froots_0',['NO_ROOTS',['../solver_8h.html#a4bb452ab79aaa95e2cf7f2453824a09dab9a321da349ac1355b717745736ad90c',1,'solver.h']]]
+];
---
+++ html/search/enumvalues_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['one_5froot_0',['ONE_ROOT',['../solver_8h.html#a4bb452ab79aaa95e2cf7f2453824a09da6700c0f6efd23571cb22aefd2510b1cf',1,'solver.h']]]
+];
